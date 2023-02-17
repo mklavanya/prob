8fd06523bf732ada1a3d26cda0ddea43c832eca0
mklavanya <lavanya79043@gmail.com>
2023-02-17 15:25:15 +0530

    sub branch

--- problem.java
+++ problem.java
@@ -2,6 +2,11 @@ public class problem {
 
 	public static void main(String[] args) {
 		 System.out.println("welcom to java ");
+		  int a = 10;
+		  int b = 20;
+		  int c = a+b;
+		  System.out.println(c);
+		  
 
 	}
 
